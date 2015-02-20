fb6934832589febf3fc35c378df943d5d6412228
MathiasB <git@denbeke.be>
2015-02-20 17:41:41 +0100

    Added DenBekePHPUnit class

--- core/tests/ORMTest.php
+++ core/tests/ORMTest.php
@@ -15,8 +15,31 @@ class Person extends \DenBeke\ORM\ORM {
 
 }
 
+class DenBekePHPUnit extends PHPUnit_Framework_TestCase {
+    
+    /**
+     * Get a private or protected method for testing/documentation purposes.
+     * How to use for MyClass->foo():
+     *      $cls = new MyClass();
+     *      $foo = PHPUnitUtil::getPrivateMethod($cls, 'foo');
+     *      $foo->invoke($cls, $...);
+     * @param object $obj The instantiated instance of your class
+     * @param string $name The name of your private/protected method
+     * @return ReflectionMethod The method you asked for
+     */
+    
+    public static function getPrivateMethod($obj, $name) {
+      $class = new ReflectionClass($obj);
+      $method = $class->getMethod($name);
+      $method->setAccessible(true);
+      return $method;
+    }
 
-class ORMTest extends PHPUnit_Framework_TestCase {
+}
+
+
+
+class ORMTest extends DenBekePHPUnit {
     
     
     public function setUp() {
