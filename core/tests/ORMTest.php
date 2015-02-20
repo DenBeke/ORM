0463e916572c359f7d7c29acbef21fa40505303b
MathiasB <git@denbeke.be>
2015-02-20 12:18:01 +0100

    Added PHPUnit config

---
+++ core/tests/ORMTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class ORMTest extends PHPUnit_Framework_TestCase
+{
+    public function testNothing()
+    {
+        $this->assertEquals(0, 0);
+        
+        //\DenBeke\ORM\ORM::init([]);
+    }
+}
