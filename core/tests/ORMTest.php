08185164ec21d36ede8aacd5607708a73df67b03
MathiasB <git@denbeke.be>
2015-02-20 13:54:45 +0100

    Trivial tests

--- core/tests/ORMTest.php
+++ core/tests/ORMTest.php
@@ -1,11 +1,51 @@
 <?php
+/**
+ * Tests for DenBeke ORM package
+ */
 
-class ORMTest extends PHPUnit_Framework_TestCase
-{
-    public function testNothing()
-    {
-        $this->assertEquals(0, 0);
+
+class Person extends \DenBeke\ORM\ORM {
+
+    public $id;
+    public $name;
+    public $city;
+
+}
+
+
+class ORMTest extends PHPUnit_Framework_TestCase {
+    
+
+    /**
+     * Basic testing for the constructor and the assignments
+     *
+     * @test
+     */    
+    public function testConstructor() {
         
-        //\DenBeke\ORM\ORM::init([]);
+        // Person created from associative array
+        $person = new Person(['name' => 'Bob', 'city' => 'Amsterdam']);
+        
+        // Person created from stdClass
+        $c = new stdClass;
+        $c->name = 'Bob';
+        $c->city = 'Amsterdam';
+        $person2 = new Person($c);
+        
+        
+        // Some trivial tests, just to have PHPUnit display more things :)
+        $this->assertEquals($person->name, 'Bob');
+        $this->assertEquals($person2->name, 'Bob');
+        $this->assertEquals($person->city, 'Amsterdam');
+        $this->assertEquals($person2->city, 'Amsterdam');
+        
+        $person->name = 'Alice';
+        $this->assertEquals($person->name, 'Alice');
+        $this->assertEquals($person->city, 'Amsterdam');
+        
+        $person->city = 'Brussels';
+        $this->assertEquals($person->city, 'Brussels');
+        
     }
+    
 }
